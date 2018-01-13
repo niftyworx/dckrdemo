6be1a7b144ee9edea92974329d5feaa1a6935ac5
niftyworx <nifty.worx.08@gmail.com>
2018-01-13 15:14:24 +0000

    restore old

--- src/index.php
+++ src/index.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-$welcome = "Hell World 9";
-=======
-$welcome = "Hell World 7";
->>>>>>> b41c86399b63bb41fc79e6a76c3b645d7ea23825
+$welcome = "Hell World 6";
 echo $welcome;
 ?>
 
